77cc978cbffa63ddbd0f59e76f2e7f9ceb6f54e4
Kirill-2-3 <87173384+Kirill-2-3@users.noreply.github.com>
2021-08-17 18:29:41 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+$(function() {
+
+    /* Functions */
+
+    /* function getELementOnClass(link) {
+        return $(`.${link}`);
+    } */
+
+
+   /*  $(window).on("resize", function() {
+
+        let getElem = $(".js-types-practices__item");
+        let getRectangle = $(".js-types-practices__rectangle");
+
+        $(getRectangle).each(function() {
+            getHeightRectangle = $(this).height();
+            console.log(getHeightRectangle);
+        });
+
+    }) */
+
+
+});
